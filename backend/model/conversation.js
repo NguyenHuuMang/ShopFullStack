582ae91bf87badf7f0785309abebbf2435ceebd7
NguyenHuuMang <nhmang.17@gmai.com>
2023-06-28 17:16:34 +0700

    shop project

---
+++ backend/model/conversation.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const conversationSchema = new mongoose.Schema(
+  {
+    groupTitle:{
+        type: String,
+    },
+    members: {
+      type: Array,
+    },
+    lastMessage: {
+      type: String,
+    },
+    lastMessageId: {
+      type: String,
+    },
+    unread: {
+      type: Boolean,
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Conversation", conversationSchema);
